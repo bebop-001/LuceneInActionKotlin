a3881d5ad81e01a4e94fd97c6005c82e03e3330f
sjs <sjs@sonic.net>
2021-04-21 14:56:23 -0700

    Force remove of index on run for testing.

--- src/main/kotlin/index_by_line/Indexer.kt
+++ src/main/kotlin/index_by_line/Indexer.kt
@@ -15,7 +15,9 @@ import org.apache.lucene.util.Version
 import org.apache.lucene.document.Document
 import org.apache.lucene.document.Field
 
-val HOME = File(System.getenv("PWD"))
+val INDEX_HOME = File(System.getenv("PWD"), "indexes")
+val DATA_DIR = File(System.getenv("PWD") + "/data")
+
 val LUCENE_VERSION = Version.LUCENE_36
 
 private fun Document._add(field:Fieldable):Document {
@@ -41,21 +43,21 @@ class Indexer (data:Map<String,List<String>>, val indexDir: File) {
             var lineCount = 0
             var textSize = 0
             data[fName]!!.forEachIndexed { index, text ->
-                doc.add(
+                doc._add(
                     Field(
                         "fName", fName,
                         Field.Store.YES,
                         Field.Index.NOT_ANALYZED
                     )
                 )
-                doc.add(
+                ._add(
                     Field(
                         "index", lineCount.toString(),
                         Field.Store.YES,
                         Field.Index.NOT_ANALYZED
                     )
                 )
-                doc.add(
+                ._add(
                     Field(
                         "text", text,
                         Field.Store.NO,
@@ -78,28 +80,43 @@ class Indexer (data:Map<String,List<String>>, val indexDir: File) {
         @Throws(Exception::class)
         @JvmStatic
         fun main(args: Array<String>) {
-            fun mkDirsIfNeeded (dirs : String) :File {
-                var toMake = HOME
-                if (File(toMake, dirs).exists())
-                    return File(toMake, dirs)
-                dirs.split("/").filter{it.isNotEmpty()}.forEach{dir ->
-                    toMake = File(toMake, dir)
-                    if (!toMake.exists() && !toMake.mkdir())
-                        throw RuntimeException("mkdirs: Failed to create $toMake")
+            fun mkDir (dir:File) {
+                var dirs = dir.toString().split("/").filter{it.isNotEmpty()}.toMutableList()
+                var toMake = ""
+                while(dirs.size > 0) {
+                    toMake = "$toMake/${dirs[0]}"
+                    val f = File(toMake)
+                    if (!f.exists() && !f.mkdir()) {
+                        throw RuntimeException("mkdir:mkdir($f) FAILED")
+                    }
+                    dirs.removeAt(0)
                 }
-                return toMake
             }
-            val dataDir = File(System.getenv("PWD") + "/data")
-            val indexDir = mkDirsIfNeeded("indexes/${javaClass.packageName}/index")
+            fun rmDirsAndFiles(root:File) {
+                if (root.exists()) {
+                    if (root.isDirectory) {
+                        root.list().forEach { file ->
+                            val f = File(root, file)
+                            if (f.isDirectory) rmDirsAndFiles(f)
+                            else if(f.isFile) f.delete()
+                            else throw RuntimeException("recurRm:Unknown file type: $f")
+                        }
+                        root.delete()
+                    }
+                }
+            }
+            val indexDir = File(INDEX_HOME, "${javaClass.packageName}")
+            rmDirsAndFiles(indexDir)
+            mkDir(indexDir)
             if (!indexDir.exists() && !indexDir.mkdir())
                 throw RuntimeException("$indexDir doesn't exist and mkdir failed")
             // read any .txt files and save contents by
             // line in a map keyed to the filename.  Lines are "chunked" to
             // 100 chars max.  This is our test data.
-            val txtFiles = dataDir.list()
-                .filter{file -> File(dataDir, file).isFile && file.endsWith(".txt")}
+            val txtFiles = DATA_DIR.list()
+                .filter{file -> File(DATA_DIR, file).isFile && file.endsWith(".txt")}
             txtFiles.forEach{fName->
-                val file = File(dataDir, fName)
+                val file = File(DATA_DIR, fName)
                 val br = file.bufferedReader()
                 var line : String? = br.readLine()?.trim()
                 while(line != null) {
