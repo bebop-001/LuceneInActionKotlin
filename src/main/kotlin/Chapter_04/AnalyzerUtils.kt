0766819d85fe5cc283294e0496bc5c64bf954446
sjs <sjs@sonic.net>
2021-05-06 09:44:48 -0700

    Kotlinized

--- src/main/kotlin/Chapter_04/AnalyzerUtils.kt
+++ src/main/kotlin/Chapter_04/AnalyzerUtils.kt
@@ -12,169 +12,167 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific lan      
 */
-package Chapter_04;
+package Chapter_04
 
-import junit.framework.Assert;
-import org.apache.lucene.util.AttributeSource;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.SimpleAnalyzer;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.tokenattributes.TermAttribute;
-import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
-import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
-import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.util.Version;
-
-import java.io.IOException;
-import java.io.StringReader;
+import kotlin.Throws
+import java.io.IOException
+import junit.framework.Assert
+import org.apache.lucene.analysis.Analyzer
+import org.apache.lucene.analysis.tokenattributes.TermAttribute
+import org.apache.lucene.util.AttributeSource
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute
+import kotlin.jvm.JvmStatic
+import org.apache.lucene.analysis.SimpleAnalyzer
+import org.apache.lucene.analysis.TokenStream
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.util.Version
+import java.io.StringReader
+import java.lang.Exception
 
 // From chapter 4
-public class AnalyzerUtils {
-  public static void displayTokens(Analyzer analyzer,
-                                   String text) throws IOException {
-    displayTokens(analyzer.tokenStream("contents", new StringReader(text)));  //A
-  }
-
-  public static void displayTokens(TokenStream stream)
-    throws IOException {
-
-    TermAttribute term = stream.addAttribute(TermAttribute.class);
-    while(stream.incrementToken()) {
-      System.out.print("[" + term.term() + "] ");    //B
+internal object AnalyzerUtils {
+    @Throws(IOException::class)
+    fun displayTokens(
+        analyzer: Analyzer,
+        text: String?
+    ) {
+        displayTokens(analyzer.tokenStream("contents", StringReader(text))) //A
     }
-  }
-  /*
+
+    @Throws(IOException::class)
+    fun displayTokens(stream: TokenStream) {
+        val term = stream.addAttribute(TermAttribute::class.java)
+        while (stream.incrementToken()) {
+            print("[" + term.term() + "] ") //B
+        }
+    }
+
+    /*
     #A Invoke analysis process
     #B Print token text surrounded by brackets
   */
-
-  public static int getPositionIncrement(AttributeSource source) {
-    PositionIncrementAttribute attr = source.addAttribute(PositionIncrementAttribute.class);
-    return attr.getPositionIncrement();
-  }
-
-  public static String getTerm(AttributeSource source) {
-    TermAttribute attr = source.addAttribute(TermAttribute.class);
-    return attr.term();
-  }
-
-  public static String getType(AttributeSource source) {
-    TypeAttribute attr = source.addAttribute(TypeAttribute.class);
-    return attr.type();
-  }
-
-  public static void setPositionIncrement(AttributeSource source, int posIncr) {
-    PositionIncrementAttribute attr = source.addAttribute(PositionIncrementAttribute.class);
-    attr.setPositionIncrement(posIncr);
-  }
-
-  public static void setTerm(AttributeSource source, String term) {
-    TermAttribute attr = source.addAttribute(TermAttribute.class);
-    attr.setTermBuffer(term);
-  }
-
-  public static void setType(AttributeSource source, String type) {
-    TypeAttribute attr = source.addAttribute(TypeAttribute.class);
-    attr.setType(type);
-  }
-
-  public static void displayTokensWithPositions
-    (Analyzer analyzer, String text) throws IOException {
-
-    TokenStream stream = analyzer.tokenStream("contents",
-                                              new StringReader(text));
-    TermAttribute term = stream.addAttribute(TermAttribute.class);
-    PositionIncrementAttribute posIncr = stream.addAttribute(PositionIncrementAttribute.class);
-
-    int position = 0;
-    while(stream.incrementToken()) {
-      int increment = posIncr.getPositionIncrement();
-      if (increment > 0) {
-        position = position + increment;
-        System.out.println();
-        System.out.print(position + ": ");
-      }
-
-      System.out.print("[" + term.term() + "] ");
+    fun getPositionIncrement(source: AttributeSource): Int {
+        val attr = source.addAttribute(
+            PositionIncrementAttribute::class.java
+        )
+        return attr.positionIncrement
     }
-    System.out.println();
-  }
 
-  public static void displayTokensWithFullDetails(Analyzer analyzer,
-                                                  String text) throws IOException {
-
-    TokenStream stream = analyzer.tokenStream("contents",                        // #A
-                                              new StringReader(text));
-
-    TermAttribute term = stream.addAttribute(TermAttribute.class);        // #B
-    PositionIncrementAttribute posIncr =                                  // #B 
-    	stream.addAttribute(PositionIncrementAttribute.class);              // #B
-    OffsetAttribute offset = stream.addAttribute(OffsetAttribute.class);  // #B
-    TypeAttribute type = stream.addAttribute(TypeAttribute.class);        // #B
-
-    int position = 0;
-    while(stream.incrementToken()) {                                  // #C
-
-      int increment = posIncr.getPositionIncrement();                 // #D
-      if (increment > 0) {                                            // #D
-        position = position + increment;                              // #D
-        System.out.println();                                         // #D
-        System.out.print(position + ": ");                            // #D
-      }
-
-      System.out.print("[" +                                 // #E
-                       term.term() + ":" +                   // #E
-                       offset.startOffset() + "->" +         // #E
-                       offset.endOffset() + ":" +            // #E
-                       type.type() + "] ");                  // #E
+    fun getTerm(source: AttributeSource): String {
+        val attr = source.addAttribute(TermAttribute::class.java)
+        return attr.term()
     }
-    System.out.println();
-  }
-  /*
+
+    fun getType(source: AttributeSource): String {
+        val attr = source.addAttribute(TypeAttribute::class.java)
+        return attr.type()
+    }
+
+    fun setPositionIncrement(source: AttributeSource, posIncr: Int) {
+        val attr = source.addAttribute(
+            PositionIncrementAttribute::class.java
+        )
+        attr.positionIncrement = posIncr
+    }
+
+    fun setTerm(source: AttributeSource, term: String?) {
+        val attr = source.addAttribute(TermAttribute::class.java)
+        attr.setTermBuffer(term)
+    }
+
+    fun setType(source: AttributeSource, type: String?) {
+        val attr = source.addAttribute(TypeAttribute::class.java)
+        attr.setType(type)
+    }
+
+    @Throws(IOException::class)
+    fun displayTokensWithPositions(analyzer: Analyzer, text: String?) {
+        val stream = analyzer.tokenStream(
+            "contents",
+            StringReader(text)
+        )
+        val term = stream.addAttribute(TermAttribute::class.java)
+        val posIncr = stream.addAttribute(
+            PositionIncrementAttribute::class.java
+        )
+        var position = 0
+        while (stream.incrementToken()) {
+            val increment = posIncr.positionIncrement
+            if (increment > 0) {
+                position = position + increment
+                println()
+                print("$position: ")
+            }
+            print("[" + term.term() + "] ")
+        }
+        println()
+    }
+
+    @Throws(IOException::class)
+    fun displayTokensWithFullDetails(
+        analyzer: Analyzer,
+        text: String?
+    ) {
+        val stream = analyzer.tokenStream(
+            "contents",  // #A
+            StringReader(text)
+        )
+        val term = stream.addAttribute(TermAttribute::class.java) // #B
+        val posIncr =  // #B 
+            stream.addAttribute(PositionIncrementAttribute::class.java) // #B
+        val offset = stream.addAttribute(OffsetAttribute::class.java) // #B
+        val type = stream.addAttribute(TypeAttribute::class.java) // #B
+        var position = 0
+        while (stream.incrementToken()) {                                  // #C
+            val increment = posIncr.positionIncrement // #D
+            if (increment > 0) {                                            // #D
+                position = position + increment // #D
+                println() // #D
+                print("$position: ") // #D
+            }
+            print(
+                "[" +  // #E
+                    term.term() + ":" +  // #E
+                    offset.startOffset() + "->" +  // #E
+                    offset.endOffset() + ":" +  // #E
+                    type.type() + "] "
+            ) // #E
+        }
+        println()
+    }
+
+    /*
     #A Perform analysis
     #B Obtain attributes of interest
     #C Iterate through all tokens
     #D Compute position and print
     #E Print all token details
    */
-
-  public static void assertAnalyzesTo(Analyzer analyzer, String input,
-                                      String[] output) throws Exception {
-    TokenStream stream = analyzer.tokenStream("field", new StringReader(input));
-
-    TermAttribute termAttr = stream.addAttribute(TermAttribute.class);
-    for (String expected : output) {
-      Assert.assertTrue(stream.incrementToken());
-      Assert.assertEquals(expected, termAttr.term());
+    @Throws(Exception::class)
+    fun assertAnalyzesTo(
+        analyzer: Analyzer, input: String?,
+        output: Array<String?>
+    ) {
+        val stream = analyzer.tokenStream("field", StringReader(input))
+        val termAttr = stream.addAttribute(TermAttribute::class.java)
+        for (expected in output) {
+            Assert.assertTrue(stream.incrementToken())
+            Assert.assertEquals(expected, termAttr.term())
+        }
+        Assert.assertFalse(stream.incrementToken())
+        stream.close()
     }
-    Assert.assertFalse(stream.incrementToken());
-    stream.close();
-  }
 
-  public static void displayPositionIncrements(Analyzer analyzer, String text)
-    throws IOException {
-    TokenStream stream = analyzer.tokenStream("contents", new StringReader(text));
-    PositionIncrementAttribute posIncr = stream.addAttribute(PositionIncrementAttribute.class);
-    while (stream.incrementToken()) {
-      System.out.println("posIncr=" + posIncr.getPositionIncrement());
-    }   
-  }
-
-  public static void main(String[] args) throws IOException {
-    System.out.println("SimpleAnalyzer");
-    displayTokensWithFullDetails(new SimpleAnalyzer(),
-        "The quick brown fox....");
-
-    System.out.println("\n----");
-    System.out.println("StandardAnalyzer");
-    displayTokensWithFullDetails(new StandardAnalyzer(Version.LUCENE_30),
-        "I'll email you at xyz@example.com");
-  }
+    @Throws(IOException::class)
+    fun displayPositionIncrements(analyzer: Analyzer, text: String?) {
+        val stream = analyzer.tokenStream("contents", StringReader(text))
+        val posIncr = stream.addAttribute(
+            PositionIncrementAttribute::class.java
+        )
+        while (stream.incrementToken()) {
+            println("posIncr=" + posIncr.positionIncrement)
+        }
+    }
 }
-
-/*
-#1 Invoke analysis process
-#2 Output token text surrounded by brackets
-*/
-
